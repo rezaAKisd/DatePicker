2f1e58cf1e692939de171cf7bf68c5065495178c
reza Akbari <rezaakbariiosdeveloper@gmail.com>
2021-10-07 19:22:52 +0330

    subject: DateTimePicker class body: create date and picker class, for all of textfield that inherited from DateTimePicker class

--- DateTimePicker/Controller/ViewController.swift
+++ DateTimePicker/Controller/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: DateTimePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //set defualt value of textField
+        textField.setCurrentDate(date: Date())
     }
-
 
 }
 
